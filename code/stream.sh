bde2e88c501e287355255df77b3134ca53f26ca1
Sachin Raina <50929371+sachinraina112@users.noreply.github.com>
2024-01-06 20:04:34 +0530

    Update stream.sh

--- code/stream.sh
+++ code/stream.sh
@@ -2,4 +2,4 @@
 echo "Running Streamlit server"
 #/bin/bash
 echo "Serving Inference"
-streamlit run streamlit.py
+streamlit run streamlit.py --server.address 0.0.0.0 --server.port 8501 --server.fileWatcherType none --browser.gatherUsageStats false --client.showErrorDetails false --client.toolbarMode minimal
